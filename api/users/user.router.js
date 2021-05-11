36d2c8ffefa143fc9de06fc5a3d55aa9c454856f
Kunal Shah <kunal.kshah@hotmail.com>
2021-05-11 21:34:31 +0530

    changes

--- api/users/user.router.js
+++ api/users/user.router.js
@@ -5,17 +5,5 @@ router.post("/create",createUser);
 router.post("/login",login);
 router.get("/getusers",getusers);
 router.get("getuserbyid/:id",getuserbyid);
-// router.post("/",createUser);
-// router.get("/",getusers);
-// router.get("/getusersbyid/:id",getuserbyid);
-// router.get("/getusersbystatus/:status",getuserbystatus);
-// router.get("/getuserbylocationandcategory/:location&:category",getUserbyLocationAndCategory);
-// router.get("/getuserbylocation/:location",getuserByLocation);
-// router.get("/getuserbycategory/:category",getuserByCategory);
-// router.get("/getuserbysubcategory/:subcategory",getuserBySubCategory);
-// router.get("/getuserbylocationandsubcategory/:location&:subCategory",getuserBylocationandsubcategory);
-// router.get("/getuserbycategoryandsubcategory/:category&:subCategory",getuserBycategoryandsubcategory)
-// router.get("/getusersbylocationandcategoryandsubcategory/:location&:category&:subCategory",getUsersByLocationAndCategoryAndSubcategory);
-// router.patch("/:id",update);
 
 module.exports = router;
