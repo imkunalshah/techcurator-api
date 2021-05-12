00c756640bbe9f8646806ff7f1e4daae1836600a
Kunal Shah <kunal.kshah@hotmail.com>
2021-05-12 11:11:20 +0530

    changes

--- config/database.js
+++ config/database.js
@@ -4,8 +4,7 @@ const connection = createPool({
     host:process.env.DB_HOST,
     user:process.env.DB_USER,
     password:process.env.DB_PASSWORD,
-    database:process.env.DB_NAME,
-    port:process.env.DB_PORT
+    database:process.env.DB_NAME
 });
 
 module.exports = connection;
