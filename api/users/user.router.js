953a99ce13d27d14b24cf9558839b7be9349342c
Kunal Shah <kunal.kshah@hotmail.com>
2021-05-12 12:06:20 +0530

    changes

--- api/users/user.router.js
+++ api/users/user.router.js
@@ -1,10 +1,10 @@
 const {createUser,getusers,getuserbyid,login} = require('./user.controller');
-const { tokenValidator } = require("../../auth/token_validation");
+const { checkToken } = require("../../auth/token_validation");
 const router = require("express").Router();
 
 router.post("/create",createUser);
 router.post("/login",login);
-router.get("/getusers",tokenValidator,getusers);
+router.get("/getusers",checkToken,getusers);
 router.get("/getuserbyid/:id",getuserbyid);
 
 module.exports = router;
