2ad72d56112b900bf05da213b5b7c7de6ae6c84a
Kunal Shah <kunal.kshah@hotmail.com>
2021-05-12 11:15:10 +0530

    changes

--- config/database.js
+++ config/database.js
@@ -1,10 +1,10 @@
 const {createPool} = require("mysql");
-require("dotenv").config();
+//require("dotenv").config();
 const connection = createPool({
-    host:process.env.DB_HOST,
-    user:process.env.DB_USER,
-    password:process.env.DB_PASSWORD,
-    database:process.env.DB_NAME
+    host:"us-cdbr-east-03.cleardb.com",
+    user:"b3919a308628eb",
+    password:"60f903c2",
+    database:"heroku_ba9f8c247340546"
 });
 
 module.exports = connection;
