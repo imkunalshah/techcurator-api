34cc4fce823b29a8617682eefc097e059edfb775
imkunalshah <36928154+imkunalshah@users.noreply.github.com>
2021-09-11 05:37:37 +0530

    techcurator task done

--- app.js
+++ app.js
@@ -1,10 +1,10 @@
 const express = require('express');
-const dotenv = require('dotenv');
+const dotenv = require('dotenv').config();
 const morgan = require('morgan');
 const connectDB = require('./config/database');
 
 //load env vars
-dotenv.config({path:'./.env'});
+// dotenv.config({path:'./.env'});
 
 //connectDB
 connectDB();
